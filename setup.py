d89632184c5a0e9c97082adfabcd1ceb9a9fbf20
Miroslav Stampar <miroslav.stampar@gmail.com>
2022-03-28 20:04:49 +0200

    Subversion bump

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name=PACKAGE_NAME,
-      version="1.0.4",
+      version="1.0.5",
       url="https://github.com/stamparm/pcapy-ng/",
       author="Miroslav Stampar",
       author_email="miroslav@sqlmap.org",
