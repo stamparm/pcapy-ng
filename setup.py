f67793500bd4aedafd50dcbbe3e29c0e05a29914
Miroslav Stampar <miroslav.stampar@gmail.com>
2026-01-06 17:45:21 +0100

    Bump the minor version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name=PACKAGE_NAME,
-      version="1.0.9",
+      version="1.0.10",
       url="https://github.com/stamparm/pcapy-ng/",
       author="Miroslav Stampar",
       author_email="miroslav@sqlmap.org",
